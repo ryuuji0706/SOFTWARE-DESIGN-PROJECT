d4db4173d4bb2257c0c7739607bd9e3d2811e123
Angge-cyber21 <23-05473@g.batstate-u.edu.ph>
2026-04-29 15:57:51 +0800

    Update URL patterns for cashflow project
    Removed old URL configuration comments and added new URL patterns for various views.

--- cashflow/urls.py
+++ cashflow/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for cashflow project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('expenses/', views.expenses, name='expenses'),
+    path('bills/', views.bills, name='bills'),
+    path('summary/', views.summary, name='summary'),
+    path('suggestion/', views.suggestion, name='suggestion'),
+    path('add_bill/', views.add_bill, name='add_bill'),
 ]
